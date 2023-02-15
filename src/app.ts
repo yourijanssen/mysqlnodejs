c81df1933f2f593ae2f5c2cf0595831a33245845
Youri Janssen <youriMTB@outlook.com>
2023-02-15 18:09:02 +0100

    video5_9 returning 404 page

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
+
+// app.use('/add-product',(req, res, next) => {
+//     res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
+// });
+
+// app.post('/product', (req, res, next) => {
+//     console.log(req.body);
+//     res.redirect('/');
+
+// });
+
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next(); // Allows the request to continue to the next middleware in line
-});
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello from Express!</h1>");
+    res.status(404).send("<h1>Page not found</h1>");
 });
 
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
