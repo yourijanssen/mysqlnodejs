81d12d82f2ed68e4b8befc4b1e1f7b27fb5a0f1f
Youri Janssen <youriMTB@outlook.com>
2023-02-15 13:11:50 +0100

    asdfasdf

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,27 @@ import http from 'http';
 import fs from 'fs';
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1><form action="/message" method="POST"><input type="text" name="message"></body>');
+    return res.end();
+}
+if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+}
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title></head>');
+res.write('<body><h1>This is your message!</h1></body>');
+res.write('</html>');
+res.end();
 });
+
+server.listen(3000);
